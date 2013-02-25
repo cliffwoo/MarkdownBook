7ab8914bfe7f761fb7bbb3fd0cc6c0307ba39622
Cliff Woo <cliffwoo@gmail.com>
2013-02-25 13:46:13 +0800

    update gen-reame.sh

--- gen-readme.sh
+++ gen-readme.sh
@@ -1,1 +1,1 @@
-cat manuscript/en/01-introduction/01-chapter1.markdown manuscript/en/02-usage/01-chapter2.markdown  > README.md
+cat manuscript/en/01-Introduction/Chapter-01.markdown manuscript/en/02-Usage/Chapter-02.markdown  > README.md
